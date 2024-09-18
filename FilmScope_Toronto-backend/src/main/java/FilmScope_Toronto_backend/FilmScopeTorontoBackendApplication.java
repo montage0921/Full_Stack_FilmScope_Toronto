ce2499dc1d448bfb7ab073946a7378106075baf7
unknown <garyshi0921@gmail.com>
2024-09-17 22:00:09 -0400

    set up

---
+++ FilmScope_Toronto-backend/src/main/java/FilmScope_Toronto_backend/FilmScopeTorontoBackendApplication.java
@@ -0,0 +1,13 @@
+package FilmScope_Toronto_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmScopeTorontoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmScopeTorontoBackendApplication.class, args);
+	}
+
+}
---
+++ FilmScope_Toronto-backend/src/test/java/FilmScope_Toronto_backend/FilmScopeTorontoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package FilmScope_Toronto_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmScopeTorontoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
