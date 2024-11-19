8aa0d4880b8210cfd76ddb7b8e81ef66be9b43ee
unknown <garyshi0921@gmail.com>
2024-11-18 20:17:02 -0500

    initialize the backend admin

---
+++ Backend-Admin/src/main/java/FilmScope/BackendAdminApplication.java
@@ -0,0 +1,13 @@
+package FilmScope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAdminApplication.class, args);
+	}
+
+}
---
+++ Backend-Admin/src/test/java/FilmScope/BackendAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package FilmScope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
