92fdd1b137326865114a49d1841234df61cace7e
unknown <garyshi0921@gmail.com>
2024-11-19 21:59:39 -0500

    add showFilmeDto

--- Backend-Admin/src/main/java/FilmScope/dto/ShowFilmDto.java
+++ Backend-Admin/src/main/java/FilmScope/dto/ShowFilmDto.java
@@ -1,2 +1,16 @@
-package FilmScope.dto;public class ShowFilmDto {
+package FilmScope.dto;
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.util.List;
+import FilmScope.entity.Film;
+
+public class ShowFilmDto {
+    private String theatre;
+    private List<LocalDate> showDates;
+    private List<LocalTime> showTimes;
+    private List<String> films;
+    private List<String> links;
+    private List<Integer> filmIDs;
+    private Boolean published;
+    private List<Film> filmInfo;
 }
