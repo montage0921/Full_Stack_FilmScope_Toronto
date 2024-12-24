e5ae9158264dafc1762c907265c77180d0d43f65
unknown <garyshi0921@gmail.com>
2024-12-24 06:15:36 -0500

    add showdetailedDto

--- Backend-Admin/src/main/java/FilmScope/dto/ShowDetailedDto.java
+++ Backend-Admin/src/main/java/FilmScope/dto/ShowDetailedDto.java
@@ -1,2 +1,26 @@
-package FilmScope.dto;public class ShowDetailedDto {
+package FilmScope.dto;
+
+import FilmScope.entity.Film;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class ShowDetailedDto {
+    private String theatre;
+    private String showTitle;
+    private Map<String,List<List<String>>> showtimes;
+    private Boolean published;
+    private List<Film> detailedMovieInfo;
+    private List<Integer> filmIds;
+    private List<Integer> Ids;
 }
