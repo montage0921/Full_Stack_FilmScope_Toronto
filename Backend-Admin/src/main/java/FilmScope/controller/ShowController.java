a69e87327ffbf5a7165947f7d1a9e1e8244e85fe
unknown <garyshi0921@gmail.com>
2024-12-24 06:15:14 -0500

    fetch detailed show info for controller

--- Backend-Admin/src/main/java/FilmScope/controller/ShowController.java
+++ Backend-Admin/src/main/java/FilmScope/controller/ShowController.java
@@ -1,10 +1,12 @@
 package FilmScope.controller;
 
+import FilmScope.dto.ShowDetailedDto;
 import FilmScope.dto.ShowListDto;
 import FilmScope.service.ShowService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +30,12 @@ public class ShowController{
     public ResponseEntity<List<ShowListDto>> getRecentShowList(){
         List<ShowListDto> recentShows=showService.getRecentShows();
         return ResponseEntity.ok(recentShows);
+    }
+
+    // get movie detailed info by showTitle
+    @GetMapping("{showName}")
+    public ResponseEntity<ShowDetailedDto> getDetailedShowInfo(@PathVariable("showName") String showName){
+        ShowDetailedDto showDetail=showService.getDetailedInfo(showName);
+        return ResponseEntity.ok(showDetail);
     }
 }
