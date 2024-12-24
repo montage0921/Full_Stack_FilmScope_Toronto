a236b63ddd7c8d6becddd50b8e49f6738bcd0c8e
unknown <garyshi0921@gmail.com>
2024-12-24 01:59:14 -0500

    update show dto

--- Backend-Admin/src/main/java/FilmScope/dto/ShowDto.java
+++ Backend-Admin/src/main/java/FilmScope/dto/ShowDto.java
@@ -1,13 +1,16 @@
 package FilmScope.dto;
 
+import FilmScope.converter.helper.TimeLink;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
 
 
 @Getter
@@ -19,12 +22,10 @@ public class ShowDto {
     private Integer id;
     private String theatre;
     private String showTitle;
-    private LocalDate showDate;
-    private LocalTime showTime;
+    private Map<LocalDate,List<TimeLink>> showtimes;
     private String filmTitle;
     private String director;
     private Integer releaseYear;
-    private String link;
     private Integer filmId;
     private Boolean published;
 }
