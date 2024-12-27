db9c1fe8e793107f496f23b1679c25a88055d186
unknown <garyshi0921@gmail.com>
2024-12-27 10:46:45 -0500

    add in-memory admin and regular users aned their rules

--- Backend-Admin/src/main/java/FilmScope/security/SecurityConfig.java
+++ Backend-Admin/src/main/java/FilmScope/security/SecurityConfig.java
@@ -3,9 +3,14 @@ package FilmScope.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,9 +18,26 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+        http.authorizeHttpRequests(auth->auth.requestMatchers("/admin-filmscope").hasRole("ADMIN"))
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users(){
+        UserDetails admin= User.builder()
+                .username("admin")
+                .password("{noop}12345stA")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails normal_user=User.builder()
+                .username("gary")
+                .password("gary")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,normal_user);
     }
 }
