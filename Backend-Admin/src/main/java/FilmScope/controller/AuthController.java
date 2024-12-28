81d5b7b5f5c5fd8a2547f5daad57610a124184e1
unknown <garyshi0921@gmail.com>
2024-12-28 10:55:38 -0500

    finish register logic

--- Backend-Admin/src/main/java/FilmScope/controller/AuthController.java
+++ Backend-Admin/src/main/java/FilmScope/controller/AuthController.java
@@ -1,2 +1,54 @@
-package FilmScope.controller;public class AuthController {
+package FilmScope.controller;
+
+import FilmScope.dto.RegisterDto;
+import FilmScope.entity.Role;
+import FilmScope.entity.UserEntity;
+import FilmScope.repository.RoleRepository;
+import FilmScope.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("auth-filmscope")
+public class AuthController {
+    private AuthenticationManager authenticationManager;
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository,
+                          RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        if (userRepository.existsByUsername(registerDto.getUsername())){
+            return new ResponseEntity<>("Username is taken", HttpStatus.BAD_REQUEST);
+        }
+
+        UserEntity user=new UserEntity();
+        user.setUsername(registerDto.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword())); // password need to encode
+
+        Role roles=roleRepository.findByRole("USER").get();
+        user.setRoles(Collections.singletonList(roles));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok("User Register Success");
+    }
 }
--- Backend-Admin/src/main/java/FilmScope/dto/RegisterDto.java
+++ Backend-Admin/src/main/java/FilmScope/dto/RegisterDto.java
@@ -1,2 +1,13 @@
-package FilmScope.dto;public class RegisterDto {
+package FilmScope.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class RegisterDto {
+    private String username;
+    private String password;
 }
