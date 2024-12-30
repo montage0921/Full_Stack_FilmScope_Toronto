b1e8c0f3104dfdbfb45d881f386af7089527a047
unknown <garyshi0921@gmail.com>
2024-12-30 16:21:25 -0500

    init react frontend

---
+++ frontend-admin/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./index.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend-admin/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <>
+      <div className="bg-blue-400 h-16 text-2xl text-white flex justify-center items-center ">
+        FilmScope Toronto Admin
+      </div>
+    </>
+  );
+}
+
+export default Header;
