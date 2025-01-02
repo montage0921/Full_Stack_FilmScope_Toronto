763a77a4632af39670cfb788702dda6906bf3241
unknown <garyshi0921@gmail.com>
2025-01-01 20:43:22 -0500

    extends jwt token expired time

--- Backend-Admin/src/main/java/FilmScope/security/SecurityConstants.java
+++ Backend-Admin/src/main/java/FilmScope/security/SecurityConstants.java
@@ -1,2 +1,9 @@
-package FilmScope.security;public class SecurityConstants {
+package FilmScope.security;
+
+import lombok.Getter;
+
+@Getter
+public class SecurityConstants {
+    public static final long JWT_EXPIRATION=36000000;
+    public static final String JWT_SECRET="YourSecureRandomSecretKeyForJWT123";
 }
