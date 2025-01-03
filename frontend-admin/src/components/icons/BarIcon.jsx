edae7fe047ee21a0189ca276f076386288eadae3
unknown <garyshi0921@gmail.com>
2025-01-02 19:33:32 -0500

    change some attribute names of svg to clear warning in console

--- frontend-admin/src/components/icons/BarIcon.jsx
+++ frontend-admin/src/components/icons/BarIcon.jsx
@@ -6,12 +6,12 @@ function FaBar() {
       <svg
         className="fill-current w-full h-full"
         viewBox="0 0 32 32"
-        enable-background="new 0 0 32 32"
+        enableBackground="new 0 0 32 32"
         id="Glyph"
         version="1.1"
-        xml:space="preserve"
+        xmlSpace="preserve"
         xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
       >
         <path
           d="M29,16c0,1.104-0.896,2-2,2H11c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C28.104,14,29,14.896,29,16z"
